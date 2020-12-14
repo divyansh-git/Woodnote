278b61008b747f974bd07e5b3a930ffcaccce3b2
divyansh rastogi <rastogi11204@gmail.com>
2020-12-14 17:20:13 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Header/Header";
-import {Switch, Route,BrowserRouter} from "react-router-dom";
+import {Switch, Route,HashRouter} from "react-router-dom";
 import Music_Card_Container from "./Music_Card_Container/Music_Card_Container";
 import Coding_Card_Container from "./Coding_Card_Container/Coding_Card_Container";
 import DailyLife_Card_Container from "./DailyLife_Card_Container/DailyLife_Card_Container";
@@ -13,7 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
       <div className="App">
-        <BrowserRouter>
+        <HashRouter>
         <Header/>
         <Switch>
           <Route   exact path ="/" component={Home}/>
@@ -24,7 +24,7 @@ function App() {
           <Route  exact path="/Daily Life" component={DailyLife_Card_Container} />
           <Route   path={`/Daily Life/:id`} component={DailyLife_Blog_Controller}/>
         </Switch>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
 }
